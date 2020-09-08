f71d716b3e2789ef1abc6f3f9e2a1a6e4fc035c6
gsprint <sprint@gonzaga.edu>
2020-09-08 14:51:20 -0700

    Initial Commit

---
+++ OOPFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OOPFun
+//
+//  Created by Gina Sprint on 9/8/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
